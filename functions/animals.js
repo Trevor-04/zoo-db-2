c78b7cdc5c73aa039fd2a929517a1d670f737efd
abyss999 <abusivehydro@gmail.com>
2024-09-30 19:09:19 -0500

    added more animal functions

--- functions/animals.js
+++ functions/animals.js
@@ -5,3 +5,12 @@ module.exports.deleteAnimal = async function (animalID) {
     return query(`DELETE FROM Animals WHERE animal_id=${animalID}`);
 }
 
+module.exports.listAllAnimals = async function () {
+    return query(`SELECT * FROM Animal`);  
+}
+
+module.exports.getAnimalById = async function (animalID) {
+    const result = await query(`SELECT * FROM Animals WHERE animal_id=${animalID}`);
+    return result[0]; 
+}
+
