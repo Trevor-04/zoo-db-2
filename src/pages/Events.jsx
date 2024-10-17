4ab434ba170992d8f3ca4164285213e9418e6dae
Trevor-04 <trevortopolski@icloud.com>
2024-10-17 01:57:51 -0500

    oct-17-2

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
 import React from 'react'
+import EventCal from '../components/eventCal'
+
 
 function Events() {
   return (
-    <div className='w-full text-center text-white font-bold bg-[#165e229e] py-12'>
-      <h1 className="relative text-2xl md:text-4xl lg:text-5xl">Events</h1>
+    
+  <div className='w-full text-center text-white font-bold bg-[#165e229e] py-12'>
+    <h1 className="relative text-2xl md:text-4xl lg:text-5xl">Events</h1>
+    <div>
+      <div className='mt-20'>
+        <EventCal />
+      </div>
     </div>
+  </div>
   )
 }
 
