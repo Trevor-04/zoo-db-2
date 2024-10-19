19ffbe7a0e502b3fa59cdfdaee7fd60fb0386abd
Trevor-04 <trevortopolski@icloud.com>
2024-10-18 19:47:40 -0500

    oct-18

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -5,10 +5,12 @@ import EventCal from '../components/eventCal'
 function Events() {
   return (
     
-  <div className='w-full text-center text-white font-bold bg-[#165e229e] py-12'>
-    <h1 className="relative text-2xl md:text-4xl lg:text-5xl">Events</h1>
+  <div>
+    <header className='w-full text-center text-white font-bold bg-[#165e229e] py-12'>
+      <h1 className="relative text-2xl md:text-4xl lg:text-5xl">Events</h1>
+    </header>
     <div>
-      <div className='mt-20'>
+      <div className='mt-16'>
         <EventCal />
       </div>
     </div>
