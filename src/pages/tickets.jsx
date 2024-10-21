8848ac0fa53d9c1efdd935342ea405d32c8632a7
Saptarshi De <saptarshide2004@gmail.com>
2024-10-21 03:54:47 -0500

    added the basic layout of tix page

--- src/pages/tickets.jsx
+++ src/pages/tickets.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+// import React from 'react'
 
-function tickets() {
+// function tickets() {
+//   return (
+//     <div>
+//       test test
+//     </div>
+//   )
+// }
+
+// export default tickets
+import React from 'react';
+import '../components/tickets.css';
+import TicketOptions from '../components/ticket';
+function Tickets() {
   return (
-    <div>
-      test test
+  
+    <div className='flex-1'>
+      <header className='w-full text-center text-white font-bold bg-[#165e229e] py-12'>
+      <h1 className="relative text-2xl md:text-4xl lg:text-5xl">Buy Tickets</h1>
+    </header>
+    <TicketOptions /> {/* Here’s where you add the clickable box */}
     </div>
-  )
+  );
 }
 
-export default tickets
+export default Tickets;
+
