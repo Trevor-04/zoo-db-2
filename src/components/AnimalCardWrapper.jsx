60cc4cc42f1bff53dd31972d8087e8495ccbff34
abyss999 <abusivehydro@gmail.com>
2024-10-21 23:01:29 -0500

    card selection

--- src/components/AnimalCardWrapper.jsx
+++ src/components/AnimalCardWrapper.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import GiftCard from './GiftCard';
 
 // AnimalCardWrapper Component (renders multiple cards)
-const AnimalCardWrapper = () => {
+const AnimalCardWrapper = ({selectedExhibit = []}) => {
   // Array of animals with their image URLs and names
-  const animals = [
-    { imageUrl: "/cougar.jpeg", name: "Cougar" },
-    { imageUrl: "/cougar.jpeg", name: "Cougar" },
-    { imageUrl: "/cougar.jpeg", name: "Cougar" },
-    { imageUrl: "/cougar.jpeg", name: "Cougar" },
-    { imageUrl: "/cougar.jpeg", name: "Cougar" },
-    { imageUrl: "/cougar.jpeg", name: "Cougar" },
 
-    
+  
+  const defaultAnimals = [
+    { imageUrl: "/cougar.jpeg", name: "Cougar" },
     // Add more animals here
   ];
 
+  const displayAnimals = selectedExhibit.length > 0 ? selectedExhibit : defaultAnimals;
+
   return (
-    <div className="bg-white cardBlock -mt-[150px] flex justify-center flex-wrap w-full">
-      {animals.map((animal, index) => (
-        <GiftCard key={index} imageUrl={animal.imageUrl} name={animal.name} />
-      ))}
+    <div className="bg-white cardBlock -mt-[0px] flex justify-center flex-wrap w-full">
+      
+      {displayAnimals.map((animal, index) => (
+        <GiftCard key = {index} 
+          imageUrl={animal.imageUrl || "/cougar.jpeg"} 
+          name={animal.name}/>
+        ))
+      }
     </div>
   );
 };
 
 export default AnimalCardWrapper;
+
