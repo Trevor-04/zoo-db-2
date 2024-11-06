8b7b05217afc5551e43c46283aefb3f6f0bc1e94
Trevor Topolski <132789229+Trevor-04@users.noreply.github.com>
2024-11-05 22:36:36 -0600

    Update test.js

--- backend/test.js
+++ backend/test.js
@@ -1,17 +1,17 @@
-const {query, connect, disconnect} = require('./functions/database');
+// const {query, connect, disconnect} = require('./functions/database');
 
-(async() => {
-    try {
-        await connect(); 
-        console.log("Connected");
+// (async() => {
+//     try {
+//         await connect(); 
+//         console.log("Connected");
 
-        const results = await query("SHOW DATABASES"); 
-        console.log("Databases", results); 
+//         const results = await query("SHOW DATABASES"); 
+//         console.log("Databases", results); 
 
-    } catch (error) {
-        console.error("Error:", error); 
-    } finally {
-        await disconnect(); 
-    }
-})();
+//     } catch (error) {
+//         console.error("Error:", error); 
+//     } finally {
+//         await disconnect(); 
+//     }
+// })();
 
